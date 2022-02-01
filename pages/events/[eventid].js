6b6d8da2294eee483e56d3a23a04fb61a64f3264
yseij <r0790156@student.thomasmore.be>
2022-02-01 19:19:37 +0100

    event detail page gebruiken

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,9 +1,37 @@
-function EventDetailPage(){
-    return (
-    <div>
-        <h1>Event detail</h1>
-    </div>
-    );
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+function EventDetailPage() {
+  const router = useRouter();
+
+  router.query.eventId;
+
+  const eventId = router.query.eventid;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
-export default EventDetailPage
+export default EventDetailPage;
