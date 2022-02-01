2f61391a2d9141a3fe949e95b01f6fb4232874d6
yseij <r0790156@student.thomasmore.be>
2022-02-01 19:39:13 +0100

    All events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,14 @@
-function AllEventsPage(){
-    return <div>
-        <h1>All Events</h1>
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
+function AllEventsPage() {
+  const events = getAllEvents();
+
+  return (
+    <div>
+      <EventList items={events} />
     </div>
+  );
 }
 
-export default AllEventsPage
+export default AllEventsPage;
