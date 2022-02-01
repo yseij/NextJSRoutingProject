cd62a3ef3d14a867c4512eab6756b37288e65eac
yseij <r0790156@student.thomasmore.be>
2022-02-01 19:54:50 +0100

    Filteren met een form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,13 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/event-search";
 
 function AllEventsPage() {
   const events = getAllEvents();
 
   return (
     <div>
+      <EventSearch />
       <EventList items={events} />
     </div>
   );
